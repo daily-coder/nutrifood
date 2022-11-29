897d68682d555a451e979aa78cf34fc1c5d764f5
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2022-11-29 20:42:47 +0530

    feat: create Meta component

---
+++ src/components/Meta/Meta.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+function Meta({ title, description }) {
+  return (
+    <Head>
+      <meta charset="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content={description} />
+      <title>{title}</title>
+    </Head>
+  );
+}
+
+Meta.defaultProps = {
+  title: "NutriFood",
+  description:
+    "Get quality fresh fruits, vegetables, nuts and more at great price. Free shipping available.",
+};
+
+export default Meta;
