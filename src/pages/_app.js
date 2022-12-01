4b8c7edb88178ecfc0c8d92d167b51de9ff0233b
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2022-12-01 17:56:32 +0530

    feat: create Layout component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,14 @@
 import GlobalStyles from "../components/GlobalStyles";
+import Layout from "../components/Layout";
 import StyleVariables from "../components/StyleVariables";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Component {...pageProps} />
+    <Layout>
       <StyleVariables />
       <GlobalStyles />
-    </>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
