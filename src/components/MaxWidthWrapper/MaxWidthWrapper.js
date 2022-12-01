07371c803c12a1e6ab94753e796fc2a83749b102
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2022-12-01 17:19:29 +0530

    feat: create MaxWidthWrapper component

---
+++ src/components/MaxWidthWrapper/MaxWidthWrapper.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const MaxWidthWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 1024px;
+  padding: var(--space-8) var(--space-16);
+`;
+
+export default MaxWidthWrapper;
