05621cea52bc6c9f96f0d40625f23ed8ed012d9c
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2022-12-06 16:09:35 +0530

    feat: use as props to dynamically change Button tag

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styled from "styled-components";
 
 const SIZES = {
@@ -5,12 +6,24 @@ const SIZES = {
   large: "var(--space-16) var(--space-48)",
 };
 
-const Button = styled.button`
+const Wrapper = styled.button`
   padding: ${({ size }) => SIZES[size]};
   border-radius: var(--border-radius-5);
   text-transform: uppercase;
   background-color: var(--color-primary);
   color: var(--color-light);
+  // links should be inline-block just like buttons
+  display: inline-block;
+  text-decoration: none;
+  font-weight: bold;
 `;
+
+function Button({ href, type, size, children }) {
+  return (
+    <Wrapper href={href} as={href ? Link : "button"} type={type} size={size}>
+      {children}
+    </Wrapper>
+  );
+}
 
 export default Button;
