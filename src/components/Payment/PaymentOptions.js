6ea71a591704de1cd7f3a1f2da78409ae1c76532
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2022-12-20 18:03:48 +0530

    feat: add PaymentOptions component

---
+++ src/components/Payment/PaymentOptions.js
@@ -0,0 +1,32 @@
+import Button from "../Button";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: var(--space-16);
+  flex-direction: column;
+`;
+
+const PayPalButton = styled(Button)`
+  border: 1px solid;
+  color: var(--color-primary);
+  background-color: var(--color-silver-light);
+`;
+
+function PaymentOptions() {
+  return (
+    <Wrapper>
+      <Button size="medium" href="#">
+        guest checkout
+      </Button>
+      <Button size="medium" href="#">
+        member checkout
+      </Button>
+      <PayPalButton size="medium" href="#">
+        paypal
+      </PayPalButton>
+    </Wrapper>
+  );
+}
+
+export default PaymentOptions;
