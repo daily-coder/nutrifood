d9b6420dcd5b3349e2158a952e9bbdc92cacc399
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2022-12-21 14:48:47 +0530

    feat: create addToCart function

--- src/components/CartContext/CartContext.js
+++ src/components/CartContext/CartContext.js
@@ -1,17 +1,24 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useState } from "react";
 
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [cartItems, setCartItems] = useState([]);
 
-  const value = useMemo(
-    () => ({
-      cartItems,
-      setCartItems,
-    }),
-    [cartItems, setCartItems]
-  );
+  function addToCart(newCartItem) {
+    setCartItems((prevCartItems) => {
+      if (prevCartItems.some((cartItem) => cartItem.id === newCartItem.id)) {
+        return prevCartItems;
+      }
+      return [...prevCartItems, newCartItem];
+    });
+  }
+
+  const value = {
+    cartItems,
+    setCartItems,
+    addToCart,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
