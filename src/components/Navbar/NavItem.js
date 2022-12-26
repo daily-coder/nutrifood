2ac2c29cc81a8db909a680e0b0a64074af37b090
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2022-12-26 17:47:34 +0530

    feat: add hover animation for nav links

--- src/components/Navbar/NavItem.js
+++ src/components/Navbar/NavItem.js
@@ -2,12 +2,35 @@ import Link from "next/link";
 import styled from "styled-components";
 
 const NavLink = styled(Link)`
+  position: relative;
   display: inline-block;
   padding: var(--space-16);
   text-transform: uppercase;
+  transition: color var(--transition-duration) ease;
 
   &:hover {
     color: var(--color-primary);
+  }
+
+  ::before {
+    content: "";
+    position: absolute;
+    left: var(--space-16);
+    bottom: var(--space-16);
+    display: block;
+    width: calc(
+      100% - var(--space-16) * 2
+    ); // 100% - (padding-left + padding-right)
+    height: 1px;
+    transform-origin: left;
+    transform: scaleX(0);
+    will-change: transform;
+    transition: transform var(--transition-duration) ease;
+    background-color: var(--color-primary);
+  }
+
+  &:hover::before {
+    transform: scaleX(1);
   }
 `;
 
