1766eee94608bac67ebeb934d0b63b1f65a8ab82
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2022-12-31 17:39:00 +0530

    chore: configure import/order eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,15 @@ module.exports = {
   extends: ["standard", "plugin:prettier/recommended", "next/core-web-vitals"],
   rules: {
     "no-console": "warn",
-    "sort-imports": ["error", { allowSeparatedGroups: true }],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
     "import/export": "off",
     "react/no-unknown-property": "warn",
   },
