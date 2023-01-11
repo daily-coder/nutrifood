cb1d0620dfca3c3fb4b0f55401d173090b97e88d
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-01-11 13:26:07 +0530

    test: bag should display number of checkout items

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -1,5 +1,24 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+describe("bag", () => {
+  it("should display number of checkout items", () => {
+    cy.visit("/store");
+
+    cy.findAllByRole("button", { name: /add to bag/i })
+      .first()
+      .should("be.visible")
+      .click({ force: true })
+      // same item cannot be added again
+      .click({ force: true });
+
+    cy.findByLabelText(/checkout bag/i)
+      .should("have.text", "1")
+      .click();
+
+    cy.findAllByRole("button", { name: /delete/i })
+      .first()
+      .should("be.visible")
+      .click({ force: true });
+
+    // no notification badge is shown if there are no items
+    cy.findByLabelText(/checkout bag/i).should("have.text", "");
   });
 });
