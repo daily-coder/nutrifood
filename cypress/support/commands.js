773393e13b6b40c556d22a5b6301de647e2cac44
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-01-12 17:52:25 +0530

    test: create cypress command assertPayment

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,14 @@ Cypress.Commands.add("decrementQuantity", () => {
 Cypress.Commands.add("assertItemPrice", (textContent) => {
   cy.findByTestId(/checkout-item-price/i).should("have.text", textContent);
 });
+
+Cypress.Commands.add("assertPayment", (textContent) => {
+  cy.findByRole("row", { name: /subtotal/i }).should(
+    "have.text",
+    `subtotal$${textContent}`
+  );
+  cy.findByRole("row", { name: /^total/i }).should(
+    "have.text",
+    `total$${textContent}`
+  );
+});
