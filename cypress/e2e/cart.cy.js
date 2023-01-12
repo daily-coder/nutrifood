c80aafaec1d56d55ce670fe956b0abb82c825066
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-01-12 17:03:41 +0530

    test: update payment total and subtotal price

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -64,3 +64,52 @@ describe("checkout item", () => {
     });
   });
 });
+
+describe("payment", () => {
+  it("should update subtotal and total price", () => {
+    cy.visit("/store");
+
+    cy.findAllByRole("button", { name: /add to bag/i })
+      .first()
+      .should("be.visible")
+      .click({ force: true });
+
+    cy.findAllByText(/^\$\d\.\d{2}/).then((prices) => {
+      const originalPrice = Number(prices[0].textContent.slice(1));
+
+      cy.findByLabelText(/checkout bag/i).click();
+      cy.findByRole("row", { name: /subtotal/i }).as("subtotalRow");
+      cy.findByRole("row", { name: /^total/i }).as("totalRow");
+
+      // prices of subtotal and total should be same as there are no shipping costs
+      cy.get("@subtotalRow").should(
+        "have.text",
+        `subtotal${prices[0].textContent}`
+      );
+      cy.get("@totalRow").should("have.text", `total${prices[0].textContent}`);
+
+      // increment
+      cy.findByRole("button", { name: /increment item quantity/i })
+        .should("be.visible")
+        .click({
+          force: true,
+        });
+
+      cy.get("@subtotalRow").should(
+        "have.text",
+        `subtotal$${originalPrice * 2}`
+      );
+      cy.get("@totalRow").should("have.text", `total$${originalPrice * 2}`);
+
+      // decrement
+      cy.findByRole("button", { name: /decrement item quantity/i })
+        .should("be.visible")
+        .click({
+          force: true,
+        });
+
+      cy.get("@subtotalRow").should("have.text", `subtotal$${originalPrice}`);
+      cy.get("@totalRow").should("have.text", `total$${originalPrice}`);
+    });
+  });
+});
