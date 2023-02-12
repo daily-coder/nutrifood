a6c232fa4131c2e2fd7991829b1d2c63a31ea712
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-12 19:22:21 +0530

    feat: migrate throttle.js to typescript

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,7 +1,8 @@
-function throttle(func, delay) {
-  let scheduled;
-  let lastRan;
-  return function (...args) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function throttle<T extends (...args: any[]) => any>(func: T, delay: number) {
+  let scheduled: ReturnType<typeof setTimeout>;
+  let lastRan: number;
+  return function (...args: Parameters<T>) {
     if (!lastRan) {
       func(...args);
       lastRan = Date.now();
