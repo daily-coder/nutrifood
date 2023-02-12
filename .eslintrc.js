924cc432979c4d6282b0881dc631ca3b522ef93e
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-12 19:22:21 +0530

    chore: update eslint to lint typescript files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,31 @@
 module.exports = {
   extends: [
     "standard",
+    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
     "plugin:cypress/recommended",
     "plugin:import/recommended",
+    "plugin:import/typescript",
     "next/core-web-vitals",
   ],
+  settings: {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
   rules: {
     "no-console": "warn",
     "import/order": [
