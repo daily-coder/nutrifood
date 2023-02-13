7d18c164e60dd646bb7871afd62bb9ba8f5a7420
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-13 21:01:43 +0530

    feat: migrate get-image-dimensions.js to typescript

--- src/utils/get-image-dimensions.ts
+++ src/utils/get-image-dimensions.ts
@@ -2,7 +2,7 @@ import path from "path";
 
 import sizeOf from "image-size";
 
-function getImageDimensions(imagePath) {
+function getImageDimensions(imagePath: string) {
   const result = sizeOf(path.join(process.cwd(), "public", imagePath));
   return result;
 }
